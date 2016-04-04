6bc3ca4b94445c36b34e1423c214c342fe243e0a
Felix Jung <felix@jung.fm>
2016-04-04 12:11:31 +0200

    Added an alias for creating GIFs from MOVs

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -7,3 +7,8 @@ alias zshconfig='vim ~/.zshrc'
 
 # System monitoring
 alias top-cpu='top -n 10 -o cpu -O time -stats pid,command,cpu,cpu_me,time,threads,ports'
+
+# Create a gif
+movToGif() {
+  ffmpeg -i $1 -pix_fmt rgb24 -r 24 -f gif - | gifsicle --optimize=2 > $2
+}
