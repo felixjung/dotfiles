48eacb092fd368a311ea2de7080356acdd1bfe45
Felix Jung <felix@jung.fm>
2016-07-28 15:50:45 +0200

    Fix github token setup

--- git/install.sh
+++ git/install.sh
@@ -14,23 +14,28 @@ user "Enter your git name: "
 read -r name
 user "Enter your git e-mail: "
 read -r email
-user "Enter your github auth token: "
-read -r token
 
 gitconfig_path="$HOME/.gitconfig"
 
 # TODO: make this optional using some prompt
 if [ ! -f "$gitconfig_path" ]; then
-  cp ./gitconfig.example "$gitconfig_path"
+  cp "$git_dir/gitconfig.example" "$gitconfig_path"
 fi
 
 git config user.name "$name"
 git config user.email "$email"
 
-cp -f ./github_token.zsh.example ./github_token.zsh
+if ! yes_no_prompt "Set up Github token?"; then
+  exit
+fi
+
+user "Enter your github auth token: "
+read -r token
+
+cp -f "$git_dir/github_token.zsh.example" "$git_dir/github_token.zsh"
 
 if [ "$os" = "Darwin" ]; then
-  sed -i "" "s/__TOKEN__/$token/g" ./github_token.zsh
+  sed -i "" "s/__TOKEN__/$token/g" "$git_dir/github_token.zsh"
 else
-  sed -i"" "s/__TOKEN__/$token/g" ./github_token.zsh
+  sed -i"" "s/__TOKEN__/$token/g" "$git_dir/github_token.zsh"
 fi
