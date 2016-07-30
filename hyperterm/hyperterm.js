8b9210900e25284386f02248671ffc1d7041a4da
Felix Jung <felix@jung.fm>
2016-07-30 12:32:32 +0200

    Change hyperterm font settings

--- hyperterm/hyperterm.js
+++ hyperterm/hyperterm.js
@@ -1,10 +1,10 @@
 module.exports = {
   config: {
     // default font size in pixels for all tabs
-    fontSize: 12,
+    fontSize: 14,
 
     // font family with optional fallbacks
-    fontFamily: 'Hack, "DejaVu Sans Mono", "Lucida Console", monospace',
+    fontFamily: '"Hack", "DejaVu Sans Mono", "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: 'rgba(248,28,229,0.75)',
