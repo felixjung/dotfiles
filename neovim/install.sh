9040380bf77e83d64068e2c0ac1a76c5821ac8d7
Felix Jung <felix@jung.fm>
2016-11-02 18:19:22 +0100

    Add neovim pip3 and RubyGem installation

--- neovim/install.sh
+++ neovim/install.sh
@@ -13,5 +13,13 @@ if ! type "pip2" > /dev/null; then
   pip2 install --user neovim
 fi
 
+if ! type "pip3" > /dev/null; then
+  pip3 install --user neovim
+fi
+
+if ! type "gem" > /dev/null; then
+  gem install neovim
+fi
+
 # Launch Neovim and install bundles
 nvim -u "$neovim_dir/nvim/plugs.vim" +PlugInstall +qa
