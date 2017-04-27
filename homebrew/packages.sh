d13e5d9ebdcfa43c85d2f6f3d59309cebf9879fa
Felix Jung <felix@jung.fm>
2017-04-27 21:25:51 +0200

    Update standard hombrew packages

--- homebrew/packages.sh
+++ homebrew/packages.sh
@@ -1,22 +1,26 @@
-brew install git
 brew install ack
 brew install bash
-brew install graphicsmagick
-brew install zsh
-brew install node --without-npm
-brew install yarn
-brew install weechat --with-aspell --with-curl --with-perl --with-python --with-ruby --with-lua
-brew install unrar
-brew install gotags
-brew install ctags
-brew install python
-brew install pango
 brew install cairo
+brew install coreutils
+brew install ctags
 brew install fontconfig
+brew install gettext
+brew install git
+brew install git-lfs
+brew install gnu-sed
+brew install gotags
+brew install graphicsmagick
+brew install neovim --HEAD
+brew install node --without-npm
+brew install nodenv
+brew install pango
+brew install python
+brew install reattach-to-user-namespace
+brew install rename
+brew install the_silver_searcher
 brew install tidy-html5
 brew install tmux
-brew install the_silver_searcher
-brew install rename
-brew install coreutils
-brew install neovim --HEAD
-brew install reattach-to-user-namespace
+brew install unrar
+brew install weechat --with-aspell --with-curl --with-perl --with-python --with-ruby --with-lua
+brew install yarn
+brew install zsh
