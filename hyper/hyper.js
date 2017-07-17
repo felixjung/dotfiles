ace57e06bf88f320f04e2a21e7029a1dd19b5fe4
Felix Jung <jung.felix@gmail.com>
2017-07-17 21:48:02 +0200

    Set Fira Code font for Hyper and Atom.

--- hyper/hyper.js
+++ hyper/hyper.js
@@ -2,7 +2,7 @@ module.exports = {
   config: {
     // Hyper configuration
     fontSize: 14,
-    fontFamily: 'Hack, "DejaVu Sans Mono", Menlo, monospace',
+    fontFamily: '"FuraCode Nerd Font", "DejaVu Sans Mono", Menlo, monospace',
     cursorColor: 'rgba(248,28,229,0.75)',
     cursorShape: 'BLOCK',
     borderColor: '#333',
