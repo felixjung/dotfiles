117ac39ce4e11497c4760ea3a684565103de2071
Felix Jung <felix@jung.fm>
2017-07-17 20:24:37 +0200

    Fix zsh history search config.

--- zsh/plugs.zsh
+++ zsh/plugs.zsh
@@ -1,7 +1,13 @@
 zplug denysdovhan/spaceship-zsh-theme, use:spaceship.zsh, from:github, as:theme
-zplug "zsh-users/zsh-history-substring-search"
 zplug "plugins/git",   from:oh-my-zsh
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug "zsh-users/zsh-history-substring-search"
+
+# History search keyboard shortcuts
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
