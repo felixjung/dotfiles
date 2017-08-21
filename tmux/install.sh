b110c2eacf5065b700b42fb58d37b7c18b5943ff
Felix Jung <jung.felix@gmail.com>
2017-08-21 22:47:16 +0200

    Add tmux plugin manager.

--- tmux/install.sh
+++ tmux/install.sh
@@ -11,3 +11,9 @@ fi
 info "Installing tmuxinator..."
 gem install tmuxinator
 success "Installed tmuxinator."
+
+info "Installing tmux plugin manager..."
+tpm_dir="$HOME/.tmux/plugins"
+mkdir -p tpm_dir
+git clone https://github.com/tmux-plugins/tpm "$tpm_dir/tpm"
+success "Installed tmux plugin manager."
