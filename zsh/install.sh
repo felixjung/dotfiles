039411a57f559e18723f2b8bae7694e2a4c27393
Felix Jung <jung.felix@gmail.com>
2018-01-28 09:14:24 +0100

    Install zplug instead of zim

--- zsh/install.sh
+++ zsh/install.sh
@@ -8,8 +8,8 @@ if ! yes_no_prompt "Set up ZSH?"; then
   exit
 fi
 
-info "Installing zim"
-git clone --recursive https://github.com/Eriner/zim.git ${ZDOTDIR:-${HOME}}/.zim
+brew install zplug
+
 echo $(which zsh) | sudo tee -a /etc/shells
 chsh -s $(which zsh)
 success "Installed and set up zsh."
