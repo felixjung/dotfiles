0d268d37f18da383e73a5e6618aa79057ad5cac4
Felix Jung <jung.felix@gmail.com>
2018-05-03 23:37:02 +0200

    Update iTerm configuration

--- iterm/iterm.zsh
+++ iterm/iterm.zsh
@@ -1,4 +1,4 @@
 # Set iTerm2 titlebar color
-echo -e "\033]6;1;bg;red;brightness;40\a"
-echo -e "\033]6;1;bg;green;brightness;44\a"
-echo -e "\033]6;1;bg;blue;brightness;52\a"
+# echo -e "\033]6;1;bg;red;brightness;40\a"
+# echo -e "\033]6;1;bg;green;brightness;44\a"
+# echo -e "\033]6;1;bg;blue;brightness;52\a"
