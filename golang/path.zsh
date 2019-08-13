4b634d6d4b8a136284545fc57fec074c13c01cfc
Felix Jung <jung.felix@gmail.com>
2019-08-13 21:11:08 +0200

    Fix Go environment variables

--- golang/path.zsh
+++ golang/path.zsh
@@ -1,6 +1,7 @@
 # Set GOROOT and GOPATH
 export GOROOT="/usr/local/opt/go/libexec"
 export GOPATH="$HOME/Development/Go"
+export GOFLAGS="-mod=vendor"
 export PATH=$PATH:$GOROOT/bin
 export PATH=$PATH:$GOPATH/bin
 
