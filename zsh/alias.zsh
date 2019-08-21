f993b5de724174cd48aeb1818337b3cee72de66e
Felix Jung <jung.felix@gmail.com>
2019-08-21 08:33:19 +0200

    Use lsd

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -9,9 +9,9 @@ alias zshconfig='vim ~/.zshrc'
 
 # System monitoring
 alias top-cpu='top -n 10 -o cpu -O time -stats pid,command,cpu,cpu_me,time,threads,ports'
-alias ll='exa --long'
-alias ls='exa'
-alias tree='exa --tree --level 2'
+alias ls='lsd'
+alias ll='ls -l'
+alias tree='ls --tree --level 2'
 
 # Create a gif
 movToGif() {
