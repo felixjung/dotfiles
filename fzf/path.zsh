cc618599b3d84c42dfd52e14a5936d59341f2c8f
Felix Jung <jung.felix@gmail.com>
2019-08-21 08:53:31 +0200

    Set fzf default options

--- fzf/path.zsh
+++ fzf/path.zsh
@@ -15,3 +15,10 @@ fi
 
 # Filter with ag to respect gitignore
 export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+ --prompt="❯ "
+ --color=fg:#959dcb,bg:#292d3e,hl:#82b1ff
+ --color=fg+:#bfc7d5,bg+:#292d3e,hl+:#89ddff
+ --color=info:#697098,prompt:#c3e88d,pointer:#89ddff
+ --color=marker:#ff869a,spinner:#c792ea,header:#ff5370'
