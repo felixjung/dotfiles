576fc08b250f67f3293ea3dc3e26e3b1d53cb126
Felix Jung <jung.felix@gmail.com>
2019-08-21 21:50:34 +0200

    Move back to exa for color support

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -9,7 +9,7 @@ alias zshconfig='vim ~/.zshrc'
 
 # System monitoring
 alias top-cpu='top -n 10 -o cpu -O time -stats pid,command,cpu,cpu_me,time,threads,ports'
-alias ls='lsd'
+alias ls='exa'
 alias ll='ls -l'
 alias tree='ls --tree --level 2'
 
