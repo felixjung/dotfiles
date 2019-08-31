25404f328589853f8fca2a0f36fdcc1f7341024c
Felix Jung <jung.felix@gmail.com>
2019-08-31 09:03:04 +0200

    Don't set -mod=vendor flag for Go

--- golang/path.zsh
+++ golang/path.zsh
@@ -1,7 +1,7 @@
 # Set GOROOT and GOPATH
 export GOROOT="/usr/local/opt/go/libexec"
 export GOPATH="$HOME/Development/Go"
-export GOFLAGS="-mod=vendor"
+export GOFLAGS=""
 export PATH=$PATH:$GOROOT/bin
 export PATH=$PATH:$GOPATH/bin
 
