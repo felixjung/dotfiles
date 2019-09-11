bac3824662240a36cb87525d0871257d6f6fe64a
Felix Jung <felix.jung@sumup.com>
2019-09-11 11:13:33 +0200

    Set up hyperkit for minikube

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -1,1 +1,12 @@
 kubectl completion zsh > "$ZFUNCTIONS/_kubectl"
+###
+# Setting up minikube
+###
+
+# We use hyperkit as hypervisor
+minikube config set vm-driver hyperkit
+
+# As per the Hombrew formula we need to provide root access to
+# the driver.
+sudo chown root:wheel $(brew --prefix)/bin/docker-machine-driver-hyperkit
+sudo chmod u+s $(brew --prefix)/bin/docker-machine-driver-hyperkit
