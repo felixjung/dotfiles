8b6e958d302135c4a65b64c42d39933f319c68b1
Felix Jung <felix.jung@sumup.com>
2021-08-25 19:41:00 +0200

    Reeconfigure fzf for homebrew

--- fzf/completion.zsh
+++ fzf/completion.zsh
@@ -1,2 +1,2 @@
 # Check if ** completion pattern exists
-[[ $- == *i* && -s "$FZF_HOME" ]] && source "$FZF_HOME/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$(brew --prefix)/opt/fzf/shell/completion.zsh" 2> /dev/null
--- fzf/path.zsh
+++ fzf/path.zsh
@@ -1,17 +1,3 @@
-# EXPORT FZF_HOME
-export FZF_HOME=${FZF_HOME:-"$HOME/.fzf"}
-
-# Set up FZF
-if [[ -s "$FZF_HOME" ]]; then
-  if [[ ! "$PATH" == *"$FZF_HOME"/bin* ]]; then
-    export PATH="$PATH:$FZF_HOME/bin"
-  fi
-
-  # Man path
-  if [[ ! "$MANPATH" == *"$FZF_HOME"/man* && -d "$FZF_HOME/man" ]]; then
-    export MANPATH="$MANPATH:$FZF_HOME/man"
-  fi
-fi
 
 # Filter with ag to respect gitignore
 export FZF_DEFAULT_COMMAND='rg --files --hidden --ignore-file ~/.ignore'
