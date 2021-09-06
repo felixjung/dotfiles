b7203c272caf0c50019e7ab36b81e98ff502ca9c
Felix Jung <felix.jung@sumup.com>
2021-09-06 23:06:42 +0200

    Fix fzf setup

--- neovim/nvim/lua/config/fzf.lua
+++ neovim/nvim/lua/config/fzf.lua
@@ -2,9 +2,8 @@ local mapping = require("lib.keymapping")
 
 require("fzf-lua").setup({
   winopts = {
-    window_on_create = function() -- nvim window options override
-      vim.cmd("set winhl=Normal:Normal") -- popup bg match normal windows
-    end,
+    hl_normal = "Normal",
+    hl_border = "FloatBorder",
   },
 })
 
