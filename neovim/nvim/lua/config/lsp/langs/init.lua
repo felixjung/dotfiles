4fe11579e3dddbf201da0e264b2f94e985cfdaee
Felix Jung <felix.jung@sumup.com>
2021-09-06 23:14:24 +0200

    Configure linting and formatting for lua

--- neovim/nvim/lua/config/lsp/langs/init.lua
+++ neovim/nvim/lua/config/lsp/langs/init.lua
@@ -2,4 +2,5 @@ return {
   go = require("config.lsp.langs.go"),
   yaml = require("config.lsp.langs.yaml"),
   typescript = require("config.lsp.langs.typescript"),
+  lua = require("config.lsp.langs.lua"),
 }
