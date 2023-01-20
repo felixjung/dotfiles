93f9c914136905ca61785633f1ecac7337fb13f3
Felix Jung <felix@sumup.com>
2023-01-20 14:20:58 +0100

    Fix fonts and light/dark

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,61 +1,43 @@
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
 
 -- A helper function for my fallback fonts
-function font_with_fallback(name, params)
-  local names = {name, "Noto Color Emoji", "FiraCode Nerd Font"}
-  return wezterm.font_with_fallback(names, params)
+local function font_with_fallback(name, params)
+	local names = { name, "Noto Color Emoji", "FiraCode Nerd Font" }
+	return wezterm.font_with_fallback(names, params)
+end
+
+local function scheme_for_appearance(appearance)
+	if appearance:find("Light") then
+		return "Catppuccin Latte"
+	else
+		return "Catppuccin Macchiato"
+	end
 end
 
 return {
-  -- Font
-  font_size = 12.0,
-  freetype_load_target = "Light",
-  line_height = 1.2,
+	-- Font
+	font_size = 12.0,
+	freetype_load_target = "Light",
+	line_height = 1.2,
 
-  font = font_with_fallback("Operator Mono SSm Lig", {weight="Light"}),
-  font_rules= {
-    -- Select a fancy italic font for italic text
-    {
-      italic = true,
-      font = font_with_fallback("Operator Mono SSm Lig", {italic=true, weight="Light"}),
-    },
+	font = font_with_fallback("Operator Mono SSm Lig", { weight = "Light" }),
 
-    -- Similarly, a fancy bold+italic font
-    {
-      italic = true,
-      intensity = "Bold",
-      font = font_with_fallback("Operator Mono SSm Lig", {italic=true, weight="DemiLight"}),
-    },
+	-- Colors
+	color_scheme_dirs = { "/Users/Felix/.config/wezterm/colors" },
+	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
 
-    -- Make regular bold text a different color to make it stand out even more
-    {
-      intensity = "Bold",
-      font = font_with_fallback("Operator Mono SSm Lig", { weight="DemiLight" }),
-    },
+	-- Behavior
+	window_close_confirmation = "NeverPrompt",
 
-    -- For half-intensity text, use a lighter weight font
-    {
-      intensity = "Half",
-      font = font_with_fallback("Operator Mono SSm Lig", {weight=325}),
-    },
-  },
-
-  -- Colors
-  color_scheme_dirs = {"/Users/Felix/.config/wezterm/colors"},
-  color_scheme = "TokyonightStorm",
-
-  -- Behavior
-  window_close_confirmation = "NeverPrompt",
-
-  -- Appearance
-  dpi = 80.0,
-  enable_tab_bar = false,
-  default_cursor_style = "SteadyUnderline",
-  window_decorations = "RESIZE",
-  window_padding = {
-    left = 20,
-    right = 20,
-    top = 20,
-    bottom = 10,
-  }
+	-- Appearance
+	dpi = 80.0,
+	enable_tab_bar = false,
+	default_cursor_style = "SteadyUnderline",
+	window_decorations = "RESIZE",
+	window_padding = {
+		left = 20,
+		right = 20,
+		top = 20,
+		bottom = 10,
+	},
 }
