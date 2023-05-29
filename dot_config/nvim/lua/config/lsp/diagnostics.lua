12b9fec468c56cb2ff8a4eac15c7e09478cad911
Felix Jung <jung.felix@gmail.com>
2023-05-29 17:21:35 +0000

    Add neovim config

---
+++ dot_config/nvim/lua/config/lsp/diagnostics.lua
@@ -0,0 +1,14 @@
+-- Automatically update diagnostics
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  underline = true,
+  update_in_insert = false,
+  virtual_text = { spacing = 4, prefix = "●" },
+  severity_sort = true,
+})
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
