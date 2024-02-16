50507c8d7359d0ca31c505eafce7080a3f0f1831
Felix Jung <felixjung@users.noreply.github.com>
2024-02-16 10:21:36 +0100

    Merge more changes from lazyvim branch (#4)

--- neovim/nvim/lua/plugins/disabled.lua
+++ neovim/nvim/lua/plugins/disabled.lua
@@ -1,19 +1,15 @@
 return {
-  { "folke/flash.nvim", enabled = false },
-  {
-    "echasnovski/mini.indentscope",
-    enabled = false,
-  },
-  {
-    "rcarriga/nvim-notify",
-    opts = {
-      level = "WARN",
-      render = "compact",
-    },
-    enabled = false,
-  },
-  -- {
-  --   "jose-elias-alvarez/null-ls.nvim",
-  --   enabled = false,
-  -- },
+	{ "folke/flash.nvim", enabled = false },
+	{
+		"echasnovski/mini.indentscope",
+		enabled = false,
+	},
+	{
+		"rcarriga/nvim-notify",
+		opts = {
+			level = "WARN",
+			render = "compact",
+		},
+		enabled = false,
+	},
 }
