421d54cb1ed159e4944304f66f2e858979cb2db7
Felix Jung <felix.jung@sumup.com>
2024-02-17 13:36:49 +0100

    Update nvim based on main

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,43 +1,21 @@
-local mapping = require("lib.keymapping")
+-- neovim/tmux navigation
+vim.keymap.set({ "n", "t" }, "<C-h>", "<Cmd>NavigatorLeft<CR>", { desc = "Go to left window" })
+vim.keymap.set({ "n", "t" }, "<C-l>", "<Cmd>NavigatorRight<CR>", { desc = "Go to right window" })
+vim.keymap.set({ "n", "t" }, "<C-k>", "<Cmd>NavigatorUp<CR>", { desc = "Go to upper window" })
+vim.keymap.set({ "n", "t" }, "<C-j>", "<Cmd>NavigatorDown<CR>", { desc = "Go to lower window" })
+vim.keymap.set({ "n", "t" }, "<C-p>", "<Cmd>NavigatorPrevious<CR>", { desc = "Go to previous window" })
 
-mapping.map("", "<c-j>", "<c-w>j<c-w>_")
-mapping.map("", "<c-k>", "<c-w>k<c-w>_")
-mapping.map("", "<c-l>", "<c-w>l<c-w>_")
-mapping.map("", "<c-h>", "<c-w>h<c-w>_")
+-- gitlinker
+vim.keymap.set(
+  { "n" },
+  "<leader>gy",
+  "<cmd>lua require('gitlinker').get_buf_range_url('n')<cr>",
+  { desc = "Link to git repo" }
+)
 
--- Navigate visual line breaks
-mapping.nnoremap("k", "gk")
-mapping.nnoremap("j", "gj")
-mapping.nnoremap("0", "g0")
-mapping.nnoremap("$", "g$")
-
--- Easier access to <ESC>
-mapping.inoremap("jj", "<Esc>")
-
--- Yank from the cursor to the end of the line, to be consistent with C and D.
-mapping.nnoremap("Y", "y$")
-
--- Normal indent
-mapping.nnoremap("<Tab>", ">>_")
-mapping.nnoremap("<S-Tab>", "<<_")
-
--- Visual shifting (does not exit Visual mode)
-mapping.vnoremap("<", "<gv")
-mapping.vnoremap(">", ">gv")
-
--- Cycle buffers
-mapping.map("", "<M-N>", ":bnext<CR>")
-mapping.map("", "<M-P>", ":bprev<CR>")
-
--- Toggle search results
-mapping.nmap("<Leader>/", ":set invhlsearch<CR>")
-
--- Undotree
-mapping.nmap("<Leader>u", ":UndotreeToggle<CR>")
-
--- Trouble
-mapping.nnoremap("<Leader>t", ":TroubleToggle<CR>")
-mapping.nnoremap("<Leader>tw", ":TroubleToggle workspace_diagnostics<CR>")
-mapping.nnoremap("<Leader>td", ":TroubleToggle document_diagnostics<CR>")
-mapping.nnoremap("<Leader>tl", ":TroubleToggle loclist<CR>")
-mapping.nnoremap("<Leader>tq", ":TroubleToggle quickfix<CR>")
+vim.keymap.set(
+  { "v" },
+  "<leader>gy",
+  "<cmd>lua require('gitlinker').get_buf_range_url('v')<cr>",
+  { desc = "Link to git repo with lines" }
+)
