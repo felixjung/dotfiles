6c99e211ba274a571e6e13f9687c59434c8c6124
Felix Jung <felix.jung@sumup.com>
2024-02-25 13:13:03 +0100

    Copy over wezterm

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,43 @@
+local wezterm = require("wezterm")
+
+-- A helper function for my fallback fonts
+local function font_with_fallback(name, params)
+	local names = { name, "Noto Color Emoji", "FiraCode Nerd Font" }
+	return wezterm.font_with_fallback(names, params)
+end
+
+local function scheme_for_appearance(appearance)
+	if appearance:find("Light") then
+		return "TokyonightDay"
+	else
+		return "TokyonightStorm"
+	end
+end
+
+return {
+	-- Font
+	font_size = 12.0,
+	freetype_load_target = "Light",
+	line_height = 1.2,
+
+	font = font_with_fallback("Operator Mono SSm Lig", { weight = "Light" }),
+
+	-- Colors
+	color_scheme_dirs = { "/Users/Felix/.config/wezterm/colors" },
+	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
+
+	-- Behavior
+	window_close_confirmation = "NeverPrompt",
+
+	-- Appearance
+	-- dpi = 80.0,
+	enable_tab_bar = false,
+	default_cursor_style = "SteadyUnderline",
+	window_decorations = "RESIZE",
+	window_padding = {
+		left = 20,
+		right = 20,
+		top = 20,
+		bottom = 10,
+	},
+}
