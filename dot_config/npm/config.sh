c3ff56dab6222a0857e80be82a270f6a8f2a0298
Felix Jung <felix.jung@sumup.com>
2024-02-25 13:07:30 +0100

    Add npm

---
+++ dot_config/npm/config.sh
@@ -0,0 +1,4 @@
+export SUMUP_GITHUB_NPM_REGISTRY="op://Private/NPM Registry GitHub Personal Access Token/token"
+export NPM_ACCESS_TOKEN="op://Private/npm Access Token/token"
+
+alias npm="op run -- npm"
