7e8a9ce6f83315b14d167613867ade133aa4d2e3
Felix Jung <felix.jung@sumup.com>
2024-02-27 22:24:17 +0100

    Move config scripts to zshrc

--- dot_config/go/config.sh
+++
@@ -1,8 +0,0 @@
-# Populates $GOROOT and adds a zsh hook to re-configure
-# it based on the working directory.
-source "$ASDF_DATA_DIR/plugins/golang/set-env.zsh"
-
-export GOPATH="$HOME/Development/Go"
-export GOFLAGS=""
-export PATH=$PATH:$GOROOT/bin
-export PATH=$PATH:$GOPATH/bin
--- dot_config/npm/config.sh
+++
@@ -1,4 +0,0 @@
-export SUMUP_GITHUB_NPM_REGISTRY="op://Private/NPM Registry GitHub Personal Access Token/token"
-export NPM_ACCESS_TOKEN="op://Private/npm Access Token/token"
-
-alias npm="op run -- npm"
