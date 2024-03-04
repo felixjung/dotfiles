134988657b13033d75ef82e31cafdc9fb2332c0c
Felix Jung <felix.jung@sumup.com>
2024-03-04 21:52:20 +0100

    Ensure env vars are set first

---
+++ dot_config/fish/conf.d/000-env.fish
@@ -0,0 +1,5 @@
+set -Ux XDG_CONFIG_HOME "$HOME/.config"
+
+set -U EDITOR nvim
+
+set -U LANG en_US.UTF-8
--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -5,7 +5,3 @@ fish_config theme choose "TokyoNight Storm"
 
 # Hide greeting message
 set -g fish_greeting
-
-set -U EDITOR nvim
-
-set -U LANG en_US.UTF-8
