8e34d93dcd5013e6013ae41288e3b3cef5118e4e
Felix Jung <felix.jung@sumup.com>
2024-03-04 21:52:43 +0100

    Try and fix Go and asdf

--- dot_config/fish/conf.d/asdf.fish
+++
@@ -1,8 +0,0 @@
-# Load asdf plugin
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
-
-set -U ASDF_DATA_DIR "$HOME/.asdf"
-set -U ASDF_CONFIG_FILE "$XDG_CONFIG_HOME/asdf/.asdfrc"
-
-# Plugins
-source ~/.asdf/plugins/golang/set-env.fish
