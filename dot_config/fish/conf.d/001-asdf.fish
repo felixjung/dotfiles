1af11f2934afd322316a03b73422987d2f59b8a7
Felix Jung <felix.jung@sumup.com>
2024-03-06 21:38:09 +0100

    Only use plugin to configure Go

--- dot_config/fish/conf.d/001-asdf.fish
+++ dot_config/fish/conf.d/001-asdf.fish
@@ -1,4 +1,3 @@
-
 set -U ASDF_DATA_DIR "$HOME/.asdf"
 set -U ASDF_CONFIG_FILE "$XDG_CONFIG_HOME/asdf/.asdfrc"
 
@@ -7,8 +6,3 @@ source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
 # Plugins
 source ~/.asdf/plugins/golang/set-env.fish
-
-go env -w GOPATH=(asdf where golang)/packages
-
-set -gx GOROOT (go env GOROOT)
-set -gx GOPATH (go env GOPATH)
