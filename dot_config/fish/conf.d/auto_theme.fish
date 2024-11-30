7430751e77266b2ceee6c0ef5ed0ff6ed482269c
Felix Jung <felix.jung@sumup.com>
2024-11-30 12:16:01 +0100

    Work on stuff

--- dot_config/fish/conf.d/auto_theme.fish
+++ dot_config/fish/conf.d/auto_theme.fish
@@ -3,9 +3,15 @@ function update_theme --on-variable macos_theme
         fish_config theme choose "TokyoNight Storm"
         set -Ux BAT_THEME tokyonight_storm
         set -xU FZF_DEFAULT_OPTS_FILE "$XDG_CONFIG_HOME/fzf/fzfrc_dark"
+
+        tmux set -g @tokyo-night-tmux_theme storm
     else if [ "$macos_theme" = light ]
         fish_config theme choose "TokyoNight Day"
         set -Ux BAT_THEME tokyonight_day
         set -xU FZF_DEFAULT_OPTS_FILE "$XDG_CONFIG_HOME/fzf/fzfrc_light"
+
+        tmux set -g @tokyo-night-tmux_theme day
     end
+
+    tmux source-file "$XDG_CONFIG_HOME/tmux/tmux.conf"
 end
