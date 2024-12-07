aed80616348ea88991e7de2a6f49859ca7884fb7
Felix Jung <felix.jung@sumup.com>
2024-12-07 12:55:34 +0100

    Improve neovim config further

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -1,159 +1,167 @@
 return {
-    {
-        "mrjones2014/smart-splits.nvim",
-        lazy = false,
-        keys = {
-            {
-                "<A-h>",
-                function()
-                    require("smart-splits").resize_left()
-                end,
-                mode = { "n" },
-                desc = "Resize Left",
-            },
-            {
-                "<A-l>",
-                function()
-                    require("smart-splits").resize_right()
-                end,
-                mode = { "n" },
-                desc = "Resize Right",
-            },
-            {
-                "<A-j>",
-                function()
-                    require("smart-splits").resize_down()
-                end,
-                mode = { "n" },
-                desc = "Resize Down",
-            },
-            {
-                "<A-k>",
-                function()
-                    require("smart-splits").resize_up()
-                end,
-                mode = { "n" },
-                desc = "Resize Up",
-            },
-            -- moving between splits
-            {
-                "<C-h>",
-                function()
-                    require("smart-splits").move_cursor_left()
-                end,
-                mode = { "n" },
-                desc = "",
-            },
-            {
-                "<C-j>",
-                function()
-                    require("smart-splits").move_cursor_down()
-                end,
-                mode = { "n" },
-                desc = "",
-            },
-            {
-                "<C-k>",
-                function()
-                    require("smart-splits").move_cursor_up()
-                end,
-                mode = { "n" },
-                desc = "",
-            },
-            {
-                "<C-l>",
-                function()
-                    require("smart-splits").move_cursor_right()
-                end,
-                mode = { "n" },
-                desc = "",
-            },
-            {
-                "<C-\\>",
-                function()
-                    require("smart-splits").move_cursor_previous()
-                end,
-                mode = { "n" },
-                desc = "",
-            },
-            -- swapping buffers between windows
-            {
-                "<leader><leader>h",
-                function()
-                    require("smart-splits").swap_buf_left()
-                end,
-                mode = { "n" },
-                desc = "",
-            },
-            {
-                "<leader><leader>j",
-                function()
-                    require("smart-splits").swap_buf_down()
-                end,
-                mode = { "n" },
-                desc = "",
-            },
-            {
-                "<leader><leader>k",
-                function()
-                    require("smart-splits").swap_buf_up()
-                end,
-                mode = { "n" },
-                desc = "",
-            },
-            {
-                "<leader><leader>l",
-                function()
-                    require("smart-splits").swap_buf_right()
-                end,
-                mode = { "n" },
-                desc = "",
-            },
-        },
+  {
+    "mrjones2014/smart-splits.nvim",
+    lazy = false,
+    keys = {
+      {
+        "<A-h>",
+        function()
+          require("smart-splits").resize_left()
+        end,
+        mode = { "n" },
+        desc = "Resize Left",
+      },
+      {
+        "<A-l>",
+        function()
+          require("smart-splits").resize_right()
+        end,
+        mode = { "n" },
+        desc = "Resize Right",
+      },
+      {
+        "<A-j>",
+        function()
+          require("smart-splits").resize_down()
+        end,
+        mode = { "n" },
+        desc = "Resize Down",
+      },
+      {
+        "<A-k>",
+        function()
+          require("smart-splits").resize_up()
+        end,
+        mode = { "n" },
+        desc = "Resize Up",
+      },
+      -- moving between splits
+      {
+        "<C-h>",
+        function()
+          require("smart-splits").move_cursor_left()
+        end,
+        mode = { "n" },
+        desc = "",
+      },
+      {
+        "<C-j>",
+        function()
+          require("smart-splits").move_cursor_down()
+        end,
+        mode = { "n" },
+        desc = "",
+      },
+      {
+        "<C-k>",
+        function()
+          require("smart-splits").move_cursor_up()
+        end,
+        mode = { "n" },
+        desc = "",
+      },
+      {
+        "<C-l>",
+        function()
+          require("smart-splits").move_cursor_right()
+        end,
+        mode = { "n" },
+        desc = "",
+      },
+      {
+        "<C-\\>",
+        function()
+          require("smart-splits").move_cursor_previous()
+        end,
+        mode = { "n" },
+        desc = "",
+      },
+      -- swapping buffers between windows
+      {
+        "<leader><leader>h",
+        function()
+          require("smart-splits").swap_buf_left()
+        end,
+        mode = { "n" },
+        desc = "",
+      },
+      {
+        "<leader><leader>j",
+        function()
+          require("smart-splits").swap_buf_down()
+        end,
+        mode = { "n" },
+        desc = "",
+      },
+      {
+        "<leader><leader>k",
+        function()
+          require("smart-splits").swap_buf_up()
+        end,
+        mode = { "n" },
+        desc = "",
+      },
+      {
+        "<leader><leader>l",
+        function()
+          require("smart-splits").swap_buf_right()
+        end,
+        mode = { "n" },
+        desc = "",
+      },
     },
-    {
-        "ruifm/gitlinker.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "S", -- Add surrounding in Normal and Visual modes
+        delete = "ds", -- Delete surrounding
+        find = "sf", -- Find surrounding (to the right)
+        find_left = "sF", -- Find surrounding (to the left)
+        highlight = "sh", -- Highlight surrounding
+        replace = "cs", -- Replace surrounding
+        update_n_lines = "sn", -- Update `n_lines`
+      },
+    },
+  },
+  {
+    "ggandor/leap.nvim",
+    enabled = false,
+    keys = {
+      { "z", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+      { "Z", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+      { "gz", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+    },
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    dependencies = {
+      {
+        "s1n7ax/nvim-window-picker",
+        version = "2.*",
         opts = {
-            add_current_line_on_normal_mode = false,
-        },
-    },
-
-    {
-        "echasnovski/mini.surround",
-        opts = {
-            mappings = {
-                add = "S",     -- Add surrounding in Normal and Visual modes
-                delete = "ds", -- Delete surrounding
-                find = "sf",   -- Find surrounding (to the right)
-                find_left = "sF", -- Find surrounding (to the left)
-                highlight = "sh", -- Highlight surrounding
-                replace = "cs", -- Replace surrounding
-                update_n_lines = "sn", -- Update `n_lines`
+          filter_rules = {
+            include_current_win = false,
+            autoselect_one = false,
+            bo = {
+              filetype = { "neo-tree", "neo-tree-popup", "notify" },
+              buftype = { "terminal", "quickfix" },
             },
+          },
         },
+      },
     },
-    {
-        "ggandor/leap.nvim",
-        enabled = false,
-        keys = {
-            { "z",  mode = { "n", "x", "o" }, desc = "Leap forward to" },
-            { "Z",  mode = { "n", "x", "o" }, desc = "Leap backward to" },
-            { "gz", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
         },
+      },
     },
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        opts = {
-            filesystem = {
-                filtered_items = {
-                    hide_dotfiles = false,
-                    hide_gitignored = false,
-                },
-            },
-        },
-    },
-    {
-        "nvim-telescope/telescope.nvim",
-    },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+  },
 }
--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -7,7 +7,42 @@ return {
       },
     },
   },
-  { "goolord/alpha-nvim", enabled = false },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        sections = {
+          { section = "header" },
+          { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+          {
+            icon = " ",
+            title = "Recent Files",
+            section = "recent_files",
+            indent = 2,
+            padding = 1,
+          },
+          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          { section = "startup" },
+        },
+      },
+    },
+    keys = {
+      {
+        "<leader>gb",
+        function()
+          Snacks.git.blame_line()
+        end,
+        desc = "Git Blame Line",
+      },
+      {
+        "<leader>gB",
+        function()
+          Snacks.gitbrowse()
+        end,
+        desc = "Git Browse",
+      },
+    },
+  },
   {
     "nvim-lualine/lualine.nvim",
     dependencies = {
