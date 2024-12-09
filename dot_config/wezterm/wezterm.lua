b99e4184cca48724e78a23b03ed7a55b30cc8ec9
Felix Jung <jung.felix@gmail.com>
2024-12-09 20:02:50 +0100

    Use resurrect for wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -169,28 +169,36 @@ config.keys = {
 
 	-- Resurrect
 	{
-		key = "w",
-		mods = "ALT",
+		key = "s",
+		mods = "LEADER",
 		action = wezterm.action_callback(function(_, _)
 			resurrect.save_state(resurrect.workspace_state.get_workspace_state())
 		end),
 	},
 	{
-		key = "W",
-		mods = "ALT",
-		action = resurrect.window_state.save_window_action(),
-	},
-	{
-		key = "T",
-		mods = "ALT",
-		action = resurrect.tab_state.save_tab_action(),
-	},
-	{
-		key = "s",
-		mods = "ALT",
-		action = wezterm.action_callback(function(_, _)
-			resurrect.save_state(resurrect.workspace_state.get_workspace_state())
-			resurrect.window_state.save_window_action()
+		key = "r",
+		mods = "LEADER",
+		action = wezterm.action_callback(function(win, pane)
+			resurrect.fuzzy_load(win, pane, function(id, label)
+				local type = string.match(id, "^([^/]+)") -- match before '/'
+				id = string.match(id, "([^/]+)$") -- match after '/'
+				id = string.match(id, "(.+)%..+$") -- remove file extention
+				local opts = {
+					relative = true,
+					restore_text = true,
+					on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+				}
+				if type == "workspace" then
+					local state = resurrect.load_state(id, "workspace")
+					resurrect.workspace_state.restore_workspace(state, opts)
+				elseif type == "window" then
+					local state = resurrect.load_state(id, "window")
+					resurrect.window_state.restore_window(pane:window(), state, opts)
+				elseif type == "tab" then
+					local state = resurrect.load_state(id, "tab")
+					resurrect.tab_state.restore_tab(pane:tab(), state, opts)
+				end
+			end)
 		end),
 	},
 }
@@ -212,5 +220,13 @@ smart_splits.apply_to_config(config, {
 		resize = "META",
 	},
 })
+
+resurrect.set_encryption({
+	enable = true,
+	method = "age",
+	private_key = wezterm.config_dir .. "../age/key.txt",
+	public_key = "age1e0yqrnkup3faajva9mzlzmmhcg39r532xek5gueskw2crjge9ypqrlyafn",
+})
+resurrect.periodic_save()
 
 return config
