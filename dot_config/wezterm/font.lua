20b800a645b395b9f16c163178f0076c91707190
Felix Jung <felix.jung@sumup.com>
2026-03-30 20:35:32 +0200

    feat(wezterm): fix operator font weight

--- dot_config/wezterm/font.lua
+++ dot_config/wezterm/font.lua
@@ -12,8 +12,7 @@ function mod.with_options(config)
 	config.font_size = 12.0
 	config.freetype_load_target = "Light"
 	config.line_height = 1.2
-	config.font =
-		font_with_fallback("Operator Mono SSm Lig", { weight = "Light" })
+	config.font = font_with_fallback("Operator Mono SSm Lig", { weight = 325 })
 end
 
 return mod
