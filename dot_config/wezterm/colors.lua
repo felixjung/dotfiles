b2615c2f81949e28e7cd06eb8f270303e1575c1b
Felix Jung <felix.jung@sumup.com>
2026-03-31 09:01:43 +0200

    feat(wezterm): set theme to Catppuccin

--- dot_config/wezterm/colors.lua
+++ dot_config/wezterm/colors.lua
@@ -2,17 +2,15 @@ local wezterm = require("wezterm")
 
 local function scheme_for_appearance(appearance)
 	if appearance:find("Light") then
-		-- return "tokyonight_day"
-		return "catppuccin-latte"
+		return "Catppuccin Latte"
 	else
-		return "catppuccin-macchiato"
+		return "Catpuccin Macchiato"
 	end
 end
 
 local mod = {}
 
 function mod.with_options(config)
-	config.color_scheme_dirs = { wezterm.config_dir .. "/colors" }
 	config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 end
 
