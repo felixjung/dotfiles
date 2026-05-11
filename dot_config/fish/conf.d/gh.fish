e5613d46d5702c5efca112506c2c4686374d4c93
Felix Jung <felix.jung@sumup.com>
2026-05-11 10:11:17 +0200

    feat: improve pr listing function

--- dot_config/fish/conf.d/gh.fish
+++ dot_config/fish/conf.d/gh.fish
@@ -1,38 +1,78 @@
-function gh_open_prs --description "List open PRs grouped by repo; supports --author, --owner, and repo filters"
+function gh_open_prs --description "List open PRs grouped by repo using GraphQL; supports --author, --owner, and repo filters"
     argparse 'a/author=' 'o/owner=' -- $argv
     or return
 
-    # Default author
     set -l author "@felixjung"
     if set -q _flag_author
         set author $_flag_author
     end
 
-    # Optional owner/org filter
-    set -l owner_args
+    set -l author_name (string replace -r '^@' '' -- $author)
+
+    set -l search_terms "is:pr state:open author:$author_name"
+
+    # Optional owner filter
     if set -q _flag_owner
-        set owner_args --owner $_flag_owner
+        # GitHub search supports org: and user:, but not a generic owner: qualifier.
+        # For simplicity, keep this as a client-side prefix filter after fetching.
+        set -l owner $_flag_owner
+    else
+        set -l owner ""
     end
 
-    # Remaining args = repos
-    set -l repo_args
+    # Optional explicit repo filters
     for repo in $argv
-        set repo_args $repo_args --repo $repo
+        set search_terms "$search_terms repo:$repo"
     end
 
-    gh search prs --author $author --state open $owner_args $repo_args \
-        --json repository,number,title,url \
-        | jq -r '
-        sort_by(.repository.nameWithOwner)
+    gh api graphql \
+        -f query='
+          query($q: String!) {
+            search(query: $q, type: ISSUE, first: 50) {
+              nodes {
+                ... on PullRequest {
+									number
+									title
+									url
+									mergeable
+									mergeStateStatus
+									repository {
+										nameWithOwner
+									}
+                }
+              }
+            }
+          }
+        ' \
+        -F q="$search_terms" \
+        | jq -r --arg owner "$owner" '
+        .data.search.nodes
+        | map(select(.repository != null))
+        | if ($owner | length) > 0
+          then map(select(.repository.nameWithOwner | startswith($owner + "/")))
+          else .
+          end
+        | sort_by(.repository.nameWithOwner, .number)
         | group_by(.repository.nameWithOwner)
         | .[]
-        | sort_by(.number)
         | "\u001b[1m\(.[0].repository.nameWithOwner)\u001b[0m\n"
           + (
               map(
                 "  \u001b[34m#\(.number)\u001b[0m "
                 + "\u001b[92m\(.title)\u001b[0m "
-                + "\u001b[2m\(.url)\u001b[0m"
+                + (
+									if .mergeStateStatus == "CLEAN" then "\u001b[32m[mergeable]\u001b[0m "
+									elif .mergeStateStatus == "HAS_HOOKS" then "\u001b[32m[mergeable]\u001b[0m "
+									elif .mergeStateStatus == "UNSTABLE" then "\u001b[33m[checks]\u001b[0m "
+									elif .mergeStateStatus == "BEHIND" then "\u001b[33m[behind]\u001b[0m "
+									elif .mergeStateStatus == "BLOCKED" then "\u001b[31m[blocked]\u001b[0m "
+									elif .mergeStateStatus == "DIRTY" then "\u001b[31m[conflicts]\u001b[0m "
+									elif .mergeable == "MERGEABLE" then "\u001b[32m[mergeable]\u001b[0m "
+									elif .mergeable == "CONFLICTING" then "\u001b[31m[conflicts]\u001b[0m "
+									else "\u001b[2m[unknown]\u001b[0m "
+									end
+                  )
+                + "\u001b[2m<\(.url)>\u001b[0m"
               )
               | join("\n")
             )
